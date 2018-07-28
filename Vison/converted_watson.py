438701a2b49109396781a50feaca25082c3215ac
Amn008 <amandalal025@Gmail.com>
2018-07-28 15:40:33 +0530

    Update converted_watson.py

--- Vison/converted_watson.py
+++ Vison/converted_watson.py
@@ -1,7 +1,7 @@
 from watson_developer_cloud import VisualRecognitionV3, WatsonApiException
 from os.path import join, dirname
 import json
-visual_recognition = VisualRecognitionV3(version='2018-03-19',iam_api_key='xRyzBKyrS6OlzRB8jhSPLxwB4vkaIG9MvU4-fmCGMsgD')
+visual_recognition = VisualRecognitionV3(version='2018-03-19',iam_api_key='')
 path = join(dirname(__file__), 'banana1.jpeg')
 with open(path, 'rb') as image_file:
 	face_result = visual_recognition.classify(images_file=image_file)
