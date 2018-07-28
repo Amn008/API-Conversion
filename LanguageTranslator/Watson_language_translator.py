3c490fd419fcf8cda84e66c4d9fd40e8ea722643
Amn008 <amandalal025@Gmail.com>
2018-07-28 15:32:51 +0530

    Update Watson_language_translator.py

--- LanguageTranslator/Watson_language_translator.py
+++ LanguageTranslator/Watson_language_translator.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import json
 from watson_developer_cloud import LanguageTranslatorV3
 
-language_translator = LanguageTranslatorV3(version='2018-05-31',username='b0c787c9-5a0e-47a9-a558-cede8d8a501a',password='CYFEqCfPFe6w')
+language_translator = LanguageTranslatorV3(version='2018-05-31',username='',password='')
 
 example_text ='Hola, cómo estás?'
 target_lang='en'
