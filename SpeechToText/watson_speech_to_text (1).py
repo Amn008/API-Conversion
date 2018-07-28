1848e9a92e96333b4f938b22b48ce17fb7060a03
Amn008 <amandalal025@Gmail.com>
2018-07-28 15:35:52 +0530

    Update watson_speech_to_text (1).py

--- SpeechToText/watson_speech_to_text (1).py
+++ SpeechToText/watson_speech_to_text (1).py
@@ -1,5 +1,5 @@
 from os.path import join, dirname
 from watson_developer_cloud import SpeechToTextV1
-cred= SpeechToTextV1(username='0e66a1df-d369-43fe-b32f-1d18c199314f',password='bIeZjddxuyWW')
+cred= SpeechToTextV1(username='',password='')
 with open(join(dirname(__file__), 'output.wav'),'rb') as source:
 	print( (((cred.recognize(audio=source,content_type='audio/wav')['results'][0])['alternatives'])[0])['transcript'])
