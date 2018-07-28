79f10b53c82a833cf882d89fc96258ed6f506a5e
Amn008 <amandalal025@Gmail.com>
2018-07-28 15:36:12 +0530

    Update watson_speech_to_text.py

--- SpeechToText/watson_speech_to_text.py
+++ SpeechToText/watson_speech_to_text.py
@@ -2,8 +2,8 @@ import json
 from os.path import join, dirname
 from watson_developer_cloud import SpeechToTextV1
 r = SpeechToTextV1(
-    username='0e66a1df-d369-43fe-b32f-1d18c199314f',
-    password='bIeZjddxuyWW',
+    username='',
+    password='',
     url='https://stream.watsonplatform.net/speech-to-text/api')
 with open(join(dirname(__file__), 'output.wav'),
           'rb') as source:
