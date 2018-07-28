6b98d3c6fca91f237d3c91f18a21da4204c25d01
Amn008 <amandalal025@Gmail.com>
2018-07-28 15:37:15 +0530

    Update converted_watson.py

--- TextToSpeech/converted_watson.py
+++ TextToSpeech/converted_watson.py
@@ -1,4 +1,4 @@
 from tts_watson.TtsWatson import TtsWatson
-ttsWatson = TtsWatson('5b8523f8-4396-41a2-b3d6-704bf47567cb', 'u0H6TzFeo3kz', 'en-US_AllisonVoice') 
+ttsWatson = TtsWatson('', '', 'en-US_AllisonVoice') 
 texttospeak='Aman Dalal'
 a=ttsWatson.play(texttospeak)
